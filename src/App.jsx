35c343c303726b8937fa487c5bbefb12fd4dcf2c
Hannah <hannahamitchell99@gmail.com>
2023-02-07 14:33:04 +0200

    tested passing one element of array into props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.scss';
 import EmployeeCard from './components/EmployeeCard';
+import team from './data/team';
 
-function App() {
+const App= () => {
   return (
     <div className="App">
-      <EmployeeCard/>
+      <EmployeeCard name = {team[0].name} role = {team[0].role}/>
     </div>
   );
 }
--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./EmployeeCard.scss"
 
-const EmployeeCard = () => {
+const EmployeeCard = (props) => {
+
+    const {name, role} = props;
   return (
     <div className = "employee-card">
-       <h1 className = "employee-card__name">Name</h1>
-       <h1 className = "employee-card__role">Role</h1>
+       <h1 className = "employee-card__name">{name}</h1>
+       <h1 className = "employee-card__role">{role}</h1>
        <div className="employee-card__counter">Counter</div>
     </div>
   )
