0faa6f78e2910e16c096d5d1d16120e2b755a87c
Hannah <hannahamitchell99@gmail.com>
2023-02-07 17:07:07 +0200

    updated Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.scss';
+import Nav from './components/Nav/Nav';
 import EmployeeCardContainer from './containers/EmployeeCardContainer';
 
 const App = () => {
   return (
     <div className="App">
+      <Nav/>
       <h1 className = "App__heading">Ticket Tracker</h1>
       <EmployeeCardContainer/>
     </div>
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Nav.scss"
+
+const Nav = () => {
+  return (
+    <nav className = "nav">
+        <label for ="nav__search">Search:</label>
+        <input type="text" className="nav__search"></input>
+        <label for="job-type">Filter by job type:</label>
+        <select name="job-type" className="job-type">
+            <option value="tester">Tester</option>
+            <option value="project-manager">Project Manager</option>
+            <option value="hard-man">Hard Man</option>
+            <option value="senior-dev">Senior Software Developer</option>
+            <option value="dev">Software Developer</option>
+            <option value="junior-dev">Junior Software Developer</option>
+        </select>
+    </nav>
+  )
+}
+
+export default Nav
