247bb6b38822095c794f9702fe9cd33050678382
Hannah <hannahamitchell99@gmail.com>
2023-02-08 18:27:43 +0200

    updated handle select function

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,25 @@ import { useState } from "react";
 import Nav from "./components/Nav/Nav";
 import EmployeeCardContainer from "./containers/EmployeeCardContainer";
 import team from "./data/team";
-import EmployeeCard from "./components/EmployeeCard/EmployeeCard";
 
 const App = () => {
-  const [input, setInput] = useState(" ");
-  const [selected, setSelected] = useState(0);
+  const [employees, setEmployees] = useState(team)
 
   const handleInput = (event) => {
-    setInput(event.target.value);
-    console.log(event.target.value);
+    const inputArray = team.filter((employee) => employee.name.includes(event.target.value))
+    setEmployees(inputArray);
   };
 
   const handleSelect = (event) => {
-    setSelected(event.target.value);
-    console.log(event.target.value);
+    const selectArray = team.filter((employee) => employee.role.includes(event.target.value))
+    setEmployees(selectArray)
   };
 
   return (
     <div className="App">
       <Nav handleInput={handleInput} handleSelect={handleSelect} />
       <h1 className="App__heading">Ticket Tracker</h1>
-      <EmployeeCardContainer/>
+      <EmployeeCardContainer team={employees} />
     </div>
   );
 };
--- src/containers/EmployeeCardContainer.jsx
+++ src/containers/EmployeeCardContainer.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import EmployeeCard from '../components/EmployeeCard/EmployeeCard'
-import team from '../data/team'
+
 import "./EmployeeCardContainer.scss"
 
-const EmployeeCardContainer = () => {
+const EmployeeCardContainer = (props) => {
+
+  const {team} = props
 
     const employeesJSX = team.map((employee) => {
         return <EmployeeCard name = {employee.name} role = {employee.role} key = {employee.id}/>
     })
-    
+
   return (
     <div className="employee-card-container">{employeesJSX}</div>
   )
@@ -17,19 +19,7 @@ const EmployeeCardContainer = () => {
 export default EmployeeCardContainer
 
 
-// const findFiltered = () => {
-//   if (team.name.includes(input)) {
-//     const returnArr = employeesJSX.filter(member => member.includes(input));
-//     return <div className="employee-card-container">{returnArr}</div>;
 
-//   } else if (team.role === selected) {
-//     const returnArr = employeesJSX.filter(member => member===selected);
-//     return <div className="employee-card-container">{returnArr}</div>;
-
-//   } else {
-//     return <div className="employee-card-container">{employeesJSX}</div>;
-//   }
-// };
 
 
 
